41c933c4f16c021bba23eeb67b7b219a253fa3ed
Rajat Talwar <rajatmtalwar@gmail.com>
2021-05-19 21:25:17 +0530

    more webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,8 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 var path = require("path");
 module.exports = {
   entry: ["./src/index.js", "./src/bar.js"],
@@ -10,13 +14,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve("public/index.html"),
     }),
+    new ExtractCssChunks({ filename: "[name].css", chunkFilename: "[id].css" }),
+    new MiniCssExtractPlugin(),
+    new OptimizeCssAssetsPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
-          "style-loader", // Injects CSS to DOM
+          // "style-loader", // Injects CSS to DOM
+          MiniCssExtractPlugin.loader,
+          // ExtractCssChunks.loader,
           "css-loader", // Loads CSS
         ],
       },
